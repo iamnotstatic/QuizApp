657a1f99185118b141373e2480dd21899d3ea466
Abdul-fatai <staticdev20046@gmail.com>
2020-01-29 14:22:38 +0100

    designed result page layout

--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -12,6 +12,60 @@ class _resultPageState extends State<resultPage> {
       appBar: AppBar(
         title: Text("Result"),
       ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 7,
+            child: Material(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      child: Container(
+                        width: 300.0,
+                        height: 300.0,
+                        child: ClipRect(
+                          child: Image(
+                            image: AssetImage("images/good.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "You scored 50 Marks",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: "Quando",
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                OutlineButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  borderSide: BorderSide(width: 3.0, color: Colors.indigo),
+                  splashColor: Colors.indigoAccent,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
